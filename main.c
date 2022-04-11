8b177be6dcc0dc6830bfa9030e7d851ac5a59a72
muzahid <bsmrstu19cse063@gmail.com>
2022-04-12 00:19:55 +0600

    search option

--- main.c
+++ main.c
@@ -4,13 +4,14 @@
 #include <string.h>
 
 #define NAME_SIZE 100
-#define BG_SIZE 5
+#define BG_SIZE 15
 #define ADDRESS_SIZE 100
-#define CONTACT_SIZE 15
+#define CONTACT_SIZE 25
 #define donarPath "data/donar.txt"
 
 FILE *donarRecord = NULL;
 
+// donar information
 typedef struct Donar
 {
     char NAME[NAME_SIZE];
@@ -19,6 +20,7 @@ typedef struct Donar
     char CONTACTNO[CONTACT_SIZE];
 } Donar;
 
+// open file
 void openFile(const char *m)
 {
     donarRecord = fopen(donarPath, m);
@@ -27,6 +29,7 @@ void openFile(const char *m)
 // ask to return main menu
 void askToReturnMainMenu()
 {
+    puts("\n\t");
     system("echo Press any key to return");
     getch();
 }
@@ -37,7 +40,7 @@ void status(char st[])
     puts("****************************************************");
     printf("\n\t%s\n\n", st);
     puts("****************************************************");
-    Sleep(1500);
+    Sleep(800);
 }
 
 void mainMenu()
@@ -52,6 +55,7 @@ void mainMenu()
     printf("\t\tPress 4 to delete data \n");
     printf("\t\tpress 5 to close program \n");
 }
+
 void insertData()
 {
     getchar();
@@ -75,11 +79,37 @@ void insertData()
     openFile("a");
     fwrite(&userData, sizeof(Donar), 1, donarRecord);
     fclose(donarRecord);
-    status("The Data Is successfully Stored");
+    status("****The Data Is successfully Stored****");
 }
+
+void searchResult(char bg[])
+{
+    int count = 1, isDonarExist = 0;
+    Donar temInfo;
+    openFile("r");
+    printf("   %-35s%-20s%-30s%-20s\n", "Name", "Blood Group", "Address", "Contact no.");
+    puts("----------------------------------------------------------------------------------------------------");
+    while (fread(&temInfo, sizeof(Donar), 1, donarRecord) == 1)
+    {
+        if (!strcmp(temInfo.BLOODGROUP, bg))
+        {
+            printf("%d. %-35s%-20s%-30s%-20s\n", count++, temInfo.NAME, temInfo.BLOODGROUP, temInfo.ADDRESS, temInfo.CONTACTNO);
+            puts("----------------------------------------------------------------------------------------------------");
+            isDonarExist++;
+        }
+    }
+    if (isDonarExist == 0)
+    {
+        system("cls");
+        puts("\t---------------------------------------------------------------------");
+        printf("\n\t\t\t\tData Not Found\n\n");
+        puts("\t---------------------------------------------------------------------");
+    }
+}
+
 void searchBloodGroup()
 {
-
+    int inp;
     printf("\t *****************************************\n");
     printf("\t ****** Help Me To Find Blood Doner ******\n");
     printf("\t *****************************************\n");
@@ -92,8 +122,61 @@ void searchBloodGroup()
     printf("\t\t 7. O(+ev)\n");
     printf("\t\t 8. O(-ev)\n");
     printf("\n\tInter Keyword:");
+    scanf("%d", &inp);
+    getchar();
+    system("cls");
+    printf("\t *****************************************\n");
+    printf("\t ****** Help Me To Find Blood Doner ******\n");
+    printf("\t *****************************************\n");
+
+    switch (inp)
+    {
+    case 1:
+        searchResult("A+");
+        break;
+    case 2:
+        searchResult("A-");
+
+        break;
+    case 3:
+
+        searchResult("B+");
+        break;
+
+    case 4:
+        searchResult("B-");
+        break;
+    case 5:
+        searchResult("AB+");
+
+        break;
+    case 6:
+        searchResult("AB-");
+
+        break;
+    case 7:
+        searchResult("O+");
+
+        break;
+    case 8:
+        searchResult("O-");
+
+        break;
+
+    default:
+        system("cls");
+        puts("\t\t--------------------------");
+        puts("\t\t      Invalid command");
+        puts("\t\tPlease inter right command");
+        puts("\t\t--------------------------");
+        Sleep(1000);
+        system("cls");
+        break;
+    }
+    fclose(donarRecord);
     getch();
 }
+
 void deleteData()
 {
     system("rmdir /s data");
